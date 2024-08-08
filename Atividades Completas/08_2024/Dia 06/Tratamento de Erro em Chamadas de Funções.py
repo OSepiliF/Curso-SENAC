c1b0cc88bdebb21008329ed67d45e29e6ec97f2c
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-08-08 09:13:25 -0400

    Update Tratamento de Erro em Chamadas de Funções.py

--- Atividades Completas/08_2024/Dia 06/Tratamento de Erro em Chamadas de Funções.py
+++ Atividades Completas/08_2024/Dia 06/Tratamento de Erro em Chamadas de Funções.py
@@ -2,9 +2,6 @@ def tratamento(num2):
         try:
             num2 = float(num2)
             print("Real:",num2,"\n")
-        
-        except ArithmeticError:
-            print("\nImpossivel realizar a divisão por Zero! \n")
             
         except ValueError:
             print("\nValor Inválido! \n")
