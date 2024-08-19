2937d79253e290881d5ccccc8351e6e61628d550
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-08-19 11:06:46 -0400

    Update

--- Atividades Completas/08_2024/Dia 19/Banco Ecomerce.sql
+++ Atividades Completas/08_2024/Dia 19/Banco Ecomerce.sql
@@ -28,7 +28,7 @@ create table EX2_ITEMPEDIDO(
 codpedido int not null,
 codproduto int not null,
 numeroitem int auto_increment primary key,
-volorunitario float not null,
+valorunitario float not null,
 quantidade int not null,
 foreign key (codpedido) references EX2_PEDIDO(codpedido),
 foreign key (codproduto) references EX2_PRODUTO(codproduto)
@@ -47,3 +47,44 @@ data date not null,
 quantidade int not null,
 foreign key (codproduto) references EX2_ITEMPEDIDO(codproduto)
 );
+
+insert into EX2_CLIENTE(codcliente,nome,datanascimento,cpf) value
+	(null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910"),
+    (null,"Cliente","2004-03-04","12345678910");
+	
+insert into EX2_PRODUTO(codproduto,descricao,quantidade) value
+	(null,"Descrição",1),
+    (null,"Descrição",2),
+    (null,"Descrição",10),
+    (null,"Descrição",42),
+    (null,"Descrição",1),
+    (null,"Descrição",2),
+    (null,"Descrição",7),
+    (null,"Descrição",90);
+	
+insert into EX2_PEDIDO(codpedido,codcliente,datapedido,notafiscal,valortotal) value
+	(null,1,"2024-05-03","049402073200323399330","32.00"),
+    (null,2,"2024-05-03","044020732002336999330","23.50"),
+    (null,3,"2024-05-03","040207372200239399330","92.55"),
+    (null,4,"2024-05-03","040207324002374399330","10.99"),
+    (null,5,"2024-05-03","040207320023399885330","232.05");
+	
+insert into EX2_ITEMPEDIDO(codpedido,codproduto,numeroitem,valorunitario,quantidade) value
+	(1,1,null,"60.00",2),
+	(2,2,null,"50.08",1),
+	(3,3,null,"64.50",4),
+	(4,4,null,"46.00",3),
+	(5,5,null,"42.50",5);
+    
+select * from EX2_CLIENTE;
+select * from EX2_PRODUTO;
+select * from EX2_PEDIDO;
+select * from EX2_ITEMPEDIDO;
