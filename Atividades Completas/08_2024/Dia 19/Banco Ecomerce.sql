052a91e3bf739ab818f1a94a05ab9143def84694
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-08-19 09:52:36 -0400

    Manda

--- Atividades Completas/08_2024/Dia 19/Banco Ecomerce.sql
+++ Atividades Completas/08_2024/Dia 19/Banco Ecomerce.sql
@@ -1,49 +1,49 @@
--- create database ecomerce;
+drop database ecomerce;
+create database ecomerce;
 use ecomerce;
--- drop database ecomerce;
 
 create table EX2_CLIENTE(
-cod_cliente int auto_increment primary key,
+codcliente int auto_increment primary key,
 nome varchar(255) not null,
-data_nascimento date not null,
+datanascimento date not null,
 cpf char(11) not null
 );
 
 create table EX2_PEDIDO(
-cod_pedido int auto_increment primary key,
-cod_cliente int not null,
-data_pedido date not null,
-nota_fiscal varchar(255) not null,
-valor_total float not null,
-foreign key (cod_cliente) references EX2_CLIENTE(cod_cliente)
+codpedido int auto_increment primary key,
+codcliente int not null,
+datapedido date not null,
+notafiscal varchar(255) not null,
+valortotal float not null,
+foreign key (codcliente) references EX2_CLIENTE(codcliente)
 );
 
 create table EX2_PRODUTO(
-cod_produto int auto_increment primary key,
+codproduto int auto_increment primary key,
 descricao varchar(255) not null,
 quantidade int not null
 );
 
 create table EX2_ITEMPEDIDO(
-cod_pedido int not null,
-cod_produto int not null,
-numero_item int auto_increment primary key,
-volor_unitario float not null,
+codpedido int not null,
+codproduto int not null,
+numeroitem int auto_increment primary key,
+volorunitario float not null,
 quantidade int not null,
-foreign key (cod_pedido) references EX2_PEDIDO(cod_pedido),
-foreign key (cod_produto) references EX2_PRODUTO(cod_produto)
+foreign key (codpedido) references EX2_PEDIDO(codpedido),
+foreign key (codproduto) references EX2_PRODUTO(codproduto)
 );
 
 create table EX2_LOG(
-cod_log int auto_increment primary key,
-data_log date not null,
+codlog int auto_increment primary key,
+data date not null,
 descricao varchar(255) not null
 );
 
 create table EX2_REQUISICAO_COMPRA(
-cod_requisicao_compra int auto_increment primary key,
-cod_produto int not null,
-data_requisicao date not null,
+codrequisicaocompra int auto_increment primary key,
+codproduto int not null,
+data date not null,
 quantidade int not null,
-foreign key (cod_produto) references EX2_ITEMPEDIDO(cod_produto)
+foreign key (codproduto) references EX2_ITEMPEDIDO(codproduto)
 );
