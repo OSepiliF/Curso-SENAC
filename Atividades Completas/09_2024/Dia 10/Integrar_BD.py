448fea5542348f4aa2c28d26612da438196fa049
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-09-10 11:08:27 -0400

    A

--- Atividades Completas/09_2024/Dia 10/Integrar_BD.py
+++ Atividades Completas/09_2024/Dia 10/Integrar_BD.py
@@ -1,70 +1,70 @@
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk
 import mysql.connector
 
-def verif():
-    if usuario.get() == '' or senha.get() == '':
-        messagebox.showerror('Erro', 'Preencha todas as opções.')
-    elif senha.get() == usuario.get():
-        messagebox.showerror("Erro!", "Senha e usuário não podem ser iguais.")
-    else:
-        if usuario.get() != usuario:
-             messagebox.showerror('Aviso!', 'Não há usuários com essas informações')
-        else:
-            messagebox.showinfo("Sucesso!", f"O usuário {usuario.get()} foi cadastrado.")
-        logintodb(usuario,senha)
-
-def logintodb(usuario, senha):
-    global db
-    try:
-        if senha:
-            db = mysql.connector.connect(host="10.28.2.36",
-                                         user='suporte',
-                                         password='suporte',
-                                         database="registros")
-        else:
-            db = mysql.connector.connect(host="10.28.2.36",
-                                         user='suporte',
-                                         database="registros")
-            
+def submitact():
+    user = usuario_entry.get()
+    passw = senha_entry.get()
+    text = texto_entry.get()
+  
+    print(f"The name entered by you is {user} {passw} {text}")
+  
+    logintodb(user, passw, text)
+ 
+def logintodb(user, passw, text):
+    
+    if passw:
+        db = mysql.connector.connect(host ="10.28.2.36",
+                                     user = 'suporte',
+                                     password = 'suporte',
+                                     db ="registros_db")
         cursor = db.cursor()
-
-        savequery = "SELECT * FROM STUDENT"
-
-        cursor.execute(savequery)
-        myresult = cursor.fetchall()
-        for x in myresult:
-            print(x)
-        print("Query Executed successfully")
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
+         
+        savequery = "SELECT * FROM usuarios"
+        update_db = f'UPDATE usuarios SET texto = {text} WHERE usuario = {user}'
         
-    finally:
-        cursor.close()
-        db.close()
+        try:
+            cursor.execute(savequery)
+            cursor.execute(update_db, (text, user))
+            db.commit()
+            myresult = cursor.fetchall()
+           
+            for x in myresult:
+                print(x)
+            print("Query Executed successfully")
+             
+        except mysql.connector.Error as err:
+            db.rollback()
+            print(f"Error occurred: {err}")
+        finally:
+            cursor.close()
+            db.close()
 
 def abrir_tela_cadastro():
-    global usuario, senha
+    global usuario_entry, senha_entry, texto_entry
     
     root = Tk()
-    root.geometry('400x400')
+    root.geometry('400x500')
     root.title('Login')
     root.config(bg='grey18')
 
     Label(root, text="Preencha os dados abaixo:", font=("Titan One", 18, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.15, anchor="center")
 
-    Label(root, text='Usuário', font=('Titan One', 15, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.33, anchor="center")
-    usuario = Entry(root, font=('Titan One', 15, "bold"))
-    usuario.place(relx=0.5, rely=0.4, anchor="center")
+    Label(root, text='Usuário', font=('Titan One', 15, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.30, anchor="center")
+    usuario_entry = Entry(root, font=('Titan One', 15, "bold"))
+    usuario_entry.place(relx=0.5, rely=0.35, anchor="center")
 
-    Label(root, text="Senha", font=("Titan One", 15, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.53, anchor="center")
-    senha = Entry(root, font=("Titan One", 15), show="*")
-    senha.place(relx=0.5, rely=0.60, anchor="center")
+    Label(root, text="Senha", font=("Titan One", 15, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.45, anchor="center")
+    senha_entry = Entry(root, font=("Titan One", 15), show="*")
+    senha_entry.place(relx=0.5, rely=0.5, anchor="center")
+
+    Label(root, text="Texto", font=("Titan One", 15, "bold"), bg='grey18', fg='white').place(relx=0.5, rely=0.6, anchor="center")
+    texto_entry = Entry(root, font=("Titan One", 15))
+    texto_entry.place(relx=0.5, rely=0.65, anchor="center")
 
     style = ttk.Style()
     style.configure('custom.TButton', font=('Titan One', 12, "bold"), padding=(10.5), anchor="center")
-    btn = ttk.Button(root, text="Entrar", command=verif, style='custom.TButton')
+    btn = ttk.Button(root, text="Entrar", command=submitact, style='custom.TButton')
     btn.place(relx=0.5, rely=0.8, anchor="center")
 
     root.mainloop()
