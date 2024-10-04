1421e1d0524425372bae3a3b7a39154fb7e28aa6
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-10-04 11:10:28 -0400

    javaS_teste

---
+++ testes_java/js/index.js
@@ -0,0 +1,11 @@
+/*
+Aluno: Filipe
+Script feito em: 10-2024
+*/
+
+var nome, sobrenome, nome_completo;
+nome= 'Filipe';
+sobrenome = 'Oliveira';
+nome_completo = nome + ' ' + sobrenome;
+
+document.getElementById('texto').innerHTML = nome_completo;
