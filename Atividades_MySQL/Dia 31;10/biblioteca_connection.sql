08d6ed7c0bed3650dd5fa92e6d216d3e63919a9e
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-10-31 10:51:54 -0400

    connector_MySQL

---
+++ Atividades_MySQL/Dia 31;10/biblioteca_connection.sql
@@ -0,0 +1,34 @@
+drop database if exists biblioteca;
+create database biblioteca;
+use biblioteca;
+
+create table usuario(
+	id_usuario int auto_increment primary key,
+    nome varchar(100),
+    cpf char(13),
+    telefone char(20)
+);
+
+create table livro (
+	id_livro int auto_increment primary key,
+    titulo varchar(80),
+    autor varchar(50),
+    genero varchar(50),
+    status varchar(50),
+    codigo int, 
+    usuario int,
+    foreign key(usuario) references usuario(id_usuario)
+);
+
+create table emprestimo (
+	id_emprestimo int auto_increment primary key,
+	id_livro int, 
+    id_usuario int,
+    foreign key(id_livro) references livro(id_livro),
+    foreign key(id_usuario) references usuario(id_livro)
+);
+
+select * from livro;
+select * from livro join usuario on livro.usuario = usuario.id_usuario;
+
+
--- Aulas_Git/Dia 22;10/main.py
+++ Aulas_Git/Dia 22;10/main.py
@@ -10,14 +10,37 @@ conexao = mysql.connector.connect(
     database='biblioteca'
 ) 
 
+cursor = conexao.cursor()
 
-rafaela = Usuario('Rafaela','123.123.123-45','67999887766')
+# insert
+cursor.execute('''
+    insert into livro (titulo, autor, genero, status, codigo) values 
+        ("O pequeno principe","Antoine de Saint-Exupéry","Fábula","Disponivel", 123),
+        ("Dom Casmurro","Machado de Assis","Romance","Disponivel", 345),
+        ("Poliana","Eleonor H. Porter","Literatura Infantil","Disponivel", 678),
+        ("Incidente em Antares","Érico Verissimo", "Ficção distópca","Emprestado", 910),
+        ("Almanacão da turma da Mônica","Mauricio de Souza", "Literatura Infantil","Emprestado", 111);
+    
+    insert into usuario (nome, cpf, telefone) values
+        ("Rafaela","12345678910","67991234567"),
+        ("Enzo","2312312312","67993234561");
+''')
 
-dom_casmurro = Livros('Dom Casmurro','Machado de Assis', 'Romance',1)
-incidente_antares = Livros('Incidente em Antares','Érico Verissimo', 'Ficção distópca',2)
-pollyanna = Livros('Poliana','Eleanor H. porter ', 'Literatuara Infantil',3)
-almanacão_Monica = Livros('Almanacão da turma da Mônica','Mauricio de Souza', 'Literatura Infantil',4)
+# select
+cursor.execute('''
+    select * from livro;
+    select * from livro join usuario on livro.usuario = usuario.id_usuario;         
+''')
+
+#update
+cursor.execute('update livro set usuario = 1 where id_livro = 1')
+
+#delete
+cursor.execute('delete from livro where id_livro = 2')
 
 
-dom_casmurro.emprestar_livro(rafaela [dom_casmurro, incidente_antares, pollyanna, almanacão_Monica])
 
+
+conexao.commit()
+
+print(dir(conexao))
