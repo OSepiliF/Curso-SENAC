f6a6c57737e0e2b4648b8f5e47ad3412980a1e52
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-11-28 18:40:03 -0400

    finalmente usuario

--- Aulas_Revisão/11_2024/Dia 11/project/controller/controller_usuario.py
+++ Aulas_Revisão/11_2024/Dia 11/project/controller/controller_usuario.py
@@ -5,35 +5,35 @@ from models.usuario import Usuario
 class Controller_Usuario:
     def __init__(self, window):
         self.window = window
-        self.db = Database("10.28.2.36", "suporte", "suporte", "biblioteca")
+        # self.db = Database("10.28.2.36", "suporte", "suporte", "biblioteca")
+        self.db = Database("localhost", "root", "suporte", "biblioteca")
+        self.usuario = None 
 
     def cadastrar(self, nome_completo, cpf, telefone, senha, confirmar_senha, label_error):
         if nome_completo == '' or cpf == '' or telefone == '' or senha == '' or confirmar_senha == '':
             label_error.setStyleSheet('color: red;')
             label_error.setText("Verifique se todos os campos estão preenchidos!")
         elif senha != confirmar_senha:
-            label_error.setStyleSheet('color: orange;')  
+            label_error.setStyleSheet('color: orange;')
             label_error.setText("Ambos os campos da senha devem ser iguais!")
         else:
             try:
-                print(nome_completo,cpf,telefone,senha,confirmar_senha, label_error)
+                print(nome_completo, cpf, telefone, senha, confirmar_senha, label_error)
                 self.db.conectar()
+                self.usuario = Usuario(self.db.cursor, self.db.conexao)  
 
-                if self.usuario.verificar_usuario(cpf): 
-                    label_error.setStyleSheet('color: red;')  
+                if self.usuario.verificar_usuario(cpf):
+                    label_error.setStyleSheet('color: red;')
                     label_error.setText("Usuário já existente!")
                 else:
-                    self.usuario.cadastrar_usuario(nome_completo, cpf, telefone, senha) 
-                    label_error.setStyleSheet('color: green;') 
+                    self.usuario.cadastrar_usuario(nome_completo, cpf, telefone, senha)
+                    label_error.setStyleSheet('color: green;')
                     label_error.setText("Cadastro efetuado com sucesso!")
 
             except mysql.connector.Error as err:
                 print(f"Erro ao conectar ao banco de dados: {err}")
-                label_error.setStyleSheet('color: red;')  
+                label_error.setStyleSheet('color: red;')
                 label_error.setText("Erro ao conectar ao banco de dados.")
             finally:
                 self.db.desconectar()
 
-    def cancelar(self):
-        self.db.desconectar()
-        self.window.close()
--- Aulas_Revisão/11_2024/Dia 11/project/models/usuario.py
+++ Aulas_Revisão/11_2024/Dia 11/project/models/usuario.py
@@ -4,21 +4,29 @@ class Usuario:
     def __init__(self, cursor, conexao):
         self.cursor = cursor  
         self.conexao = conexao  
+
+    def validar_cpf(self, cpf):
+        cpf = ''.join(filter(str.isdigit, cpf))  
+        return len(cpf) == 11  
         
     def verificar_usuario(self, cpf):
         query = "SELECT * FROM usuario WHERE cpf = %s"
         self.cursor.execute(query, (cpf,))
-        result = self.cursor.fetchone()
-        print('Verificou o Usuário.')  
-        return result is not None
+        return self.cursor.fetchone() is not None 
         
-    def cadastrar_usuario(self, nome_completo, cpf, telefone, senha):
+    def cadastrar_usuario(self, nome, cpf, telefone, senha):
+        if not self.validar_cpf(cpf):
+            print("Erro: CPF inválido.")
+            return False
+
         try:
-            query = "INSERT INTO usuario (nome_completo, cpf, telefone, senha) VALUES (%s, %s, %s, %s)"
-            self.cursor.execute(query, (nome_completo, cpf, telefone, senha))
+            query = "INSERT INTO usuario (nome, cpf, telefone, senha) VALUES (%s, %s, %s, %s)"
+            self.cursor.execute(query, (nome, cpf, telefone, senha))
             self.conexao.commit() 
-            print('Cadastrou o Usuário.')
+            print("Usuário cadastrado com sucesso.")
+            return True
 
         except mysql.connector.Error as err:
             print(f"Erro ao cadastrar usuário: {err}")
             self.conexao.rollback()  
+            return False
