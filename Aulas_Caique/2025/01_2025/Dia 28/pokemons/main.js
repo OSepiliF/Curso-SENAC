86a30c88589b4e9e66bf2c668c742e0a0ebb7bc0
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2025-02-03 18:53:10 -0400

    inicio pokedex

--- Aulas_Caique/2025/01_2025/Dia 28/pokemons/main.js
+++ Aulas_Caique/2025/01_2025/Dia 28/pokemons/main.js
@@ -1,38 +1,77 @@
-async function requisicao(url){
+let nextUrl = "https://pokeapi.co/api/v2/pokemon?limit=14&offset=0"; 
+let prevUrl = null;
+
+async function requisicao(url) {
     const respostaRequisicao = await fetch(url);
-    const jsonRequisicao = await (await respostaRequisicao).json();
+    const jsonRequisicao = await respostaRequisicao.json();
 
-    console.log(jsonRequisicao);
+    document.getElementById("pokemon").innerHTML = "";
 
-    const body = await jsonRequisicao;
-    const pokemon = {
-        name: body.name,
-        habilidades: body.abilities,
-        imagem: body.sprites.front_default
+    nextUrl = jsonRequisicao.next;
+    prevUrl = jsonRequisicao.previous;
+
+    for (let pokemonData of jsonRequisicao.results) {
+        const pokemonResponse = await fetch(pokemonData.url);
+        const pokemonJson = await pokemonResponse.json();
+
+        const pokemon = {
+            id: pokemonJson.id, 
+            name: pokemonJson.name,
+            habilidades: pokemonJson.abilities,
+            tipo: pokemonJson.types.map(tipo => tipo.type.name).join(', '), 
+            imagem: pokemonJson.sprites.front_default
+        };
+
+        const div = document.createElement("div");
+        div.classList.add("pokemon-card");
+
+        const nome = document.createElement("h3");
+        nome.innerText = pokemon.name;
+
+        const img = document.createElement("img");
+        img.src = pokemon.imagem;
+
+        const idDiv = document.createElement("div"); 
+        idDiv.innerText = `ID: ${pokemon.id}`;
+
+        const tipoDiv = document.createElement("div");
+        tipoDiv.innerText = `Tipo: ${pokemon.tipo}`;
+
+        const habilidadesDiv = document.createElement("div");
+        habilidadesDiv.innerText = "Habilidades: ";
+
+        pokemon.habilidades.forEach(abilities => {
+            const span = document.createElement("span");
+            span.innerText = abilities.ability.name;
+            habilidadesDiv.appendChild(span);
+        });
+
+        div.appendChild(idDiv); 
+        div.appendChild(nome);
+        div.appendChild(img);
+        div.appendChild(habilidadesDiv);
+        div.appendChild(tipoDiv);
+
+        document.getElementById("pokemon").appendChild(div);
     }
+}
 
-    const div = document.createElement("div");
-    const span = document.createElement("span");
-    const img = document.createElement("img");
-    
-    img.src = pokemon.imagem;
-    span.innerText = pokemon.habilidades[0].ability.name
+document.getElementById("proxima-btn").addEventListener("click", () => {
+    if (nextUrl) {
+        requisicao(nextUrl); 
+    }
+});
 
-    div.appendChild(img);
-    div.appendChild(span);
-    
-    document.getElementById("pokemon").appendChild(div);
+document.getElementById("voltar-btn").addEventListener("click", () => {
+    if (prevUrl) {
+        requisicao(prevUrl); 
+    }
+});
 
-    console.log(pokemon);
-}  
+document.getElementById("search-button").addEventListener("click", () => {
+    const input = document.getElementById("pokemon-input").value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${input}`; 
+    requisicao(url); 
+});
 
-requisicao("https://pokeapi.co/api/v2/pokemon/10");
-requisicao("https://pokeapi.co/api/v2/pokemon/12");
-requisicao("https://pokeapi.co/api/v2/pokemon/14");
-requisicao("https://pokeapi.co/api/v2/pokemon/16");
-requisicao("https://pokeapi.co/api/v2/pokemon/18");
-requisicao("https://pokeapi.co/api/v2/pokemon/20");
-requisicao("https://pokeapi.co/api/v2/pokemon/22");
-requisicao("https://pokeapi.co/api/v2/pokemon/24");
-requisicao("https://pokeapi.co/api/v2/pokemon/26");
-requisicao("https://pokeapi.co/api/v2/pokemon/28");
+requisicao(nextUrl);
